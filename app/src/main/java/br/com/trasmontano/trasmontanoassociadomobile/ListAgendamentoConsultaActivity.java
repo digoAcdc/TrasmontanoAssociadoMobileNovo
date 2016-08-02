67234cb42f58f76ac79411f6fd5241135b3d1993
digoacdc <barbosa_silveira@hotmail.com>
2016-08-02 07:52:18 -0300

    Agendamento de Consulta

---
+++ app/src/main/java/br/com/trasmontano/trasmontanoassociadomobile/ListAgendamentoConsultaActivity.java
@@ -0,0 +1,13 @@
+package br.com.trasmontano.trasmontanoassociadomobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListAgendamentoConsultaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_agendamento_consulta);
+    }
+}
