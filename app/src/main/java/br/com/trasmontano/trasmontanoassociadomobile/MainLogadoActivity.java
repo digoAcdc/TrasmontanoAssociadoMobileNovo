5546dbcd69f82a26099d85c3f79afaeb13bfadf0
digoacdc <barbosa_silveira@hotmail.com>
2016-08-19 14:40:16 -0300

    ajustes

--- app/src/main/java/br/com/trasmontano/trasmontanoassociadomobile/MainLogadoActivity.java
+++ app/src/main/java/br/com/trasmontano/trasmontanoassociadomobile/MainLogadoActivity.java
@@ -293,31 +293,24 @@ public class MainLogadoActivity extends AppCompatActivity
         }
     }
 
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_logado, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_central_atendimento) {
-            //Toast.makeText(MainLogadoActivity.this, "Criar pagina sobre", Toast.LENGTH_LONG).show();
-
-            Intent i = new Intent(MainLogadoActivity.this, SobreActivity.class);
+            Intent i = new Intent(MainLogadoActivity.this, CentralDeAtendimentoActivity.class);
             startActivity(i);
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     private void CentralDeAtendimento() {
         Intent i = new Intent(MainLogadoActivity.this, CentralDeAtendimentoActivity.class);
